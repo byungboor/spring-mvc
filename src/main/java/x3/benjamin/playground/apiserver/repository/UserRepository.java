9fcc3d2813f5e3c181f4254d13690d3fb018b302
Benjamin <byungboo.kim@nhnent.com>
2018-05-26 21:22:01 +0900

    lombok version update & add log

--- src/main/java/x3/benjamin/playground/apiserver/repository/UserRepository.java
+++ src/main/java/x3/benjamin/playground/apiserver/repository/UserRepository.java
@@ -1,9 +1,6 @@
 package x3.benjamin.playground.apiserver.repository;
 
 import x3.benjamin.playground.apiserver.model.User;
-
-import javax.annotation.PostConstruct;
-
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -15,18 +12,17 @@ import java.util.List;
 @Repository
 public class UserRepository {
 
+	public List<User> findAll() {
 
-    public List<User> findAll(){
+		System.out.println("Repository Layer - users method is called");
+		List<User> users = new ArrayList<>();
 
-        System.out.println("Repository Layer - users method is called");
-        List<User> users = new ArrayList<>();
+		users.add(new User(1L, "Benjmain", "Bundang"));
+		users.add(new User(2L, "Manti", "Suwon"));
+		users.add(new User(3L, "Yoda", "Suwon"));
+		users.add(new User(4L, "Dugi", "Seoul"));
+		users.add(new User(5L, "Franky", "Bundang"));
 
-        users.add(new User(1L, "Benjmain", "Bundang"));
-        users.add(new User(2L, "Manti", "Suwon"));
-        users.add(new User(3L, "Yoda", "Suwon"));
-        users.add(new User(4L, "Dugi", "Seoul"));
-        users.add(new User(5L, "Franky", "Bundang"));
-
-        return users;
-    }
+		return users;
+	}
 }
