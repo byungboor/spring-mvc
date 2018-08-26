e0056b89be2c6337cf2315e82c426350344237fb
Benjamin <byungboo.kim@nhnent.com>
2018-08-26 16:19:12 +0900

    해법

--- src/main/java/x3/benjamin/playground/apiserver/config/ServletContextConfig.java
+++ src/main/java/x3/benjamin/playground/apiserver/config/ServletContextConfig.java
@@ -10,6 +10,7 @@ import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 import java.util.List;
 
 
@@ -18,22 +19,22 @@ import java.util.List;
  */
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = { "x3.benjamin.playground.apiserver.controller" })
+@ComponentScan(basePackages = {"x3.benjamin.playground.apiserver.controller"})
 public class ServletContextConfig extends WebMvcConfigurerAdapter {
 
-	@Override
-	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-		converters.add(jackson2HttpMessageConverter());
-		super.configureMessageConverters(converters);
-	}
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(jackson2HttpMessageConverter());
+        super.configureMessageConverters(converters);
+    }
 
-	@Bean
-	public MappingJackson2HttpMessageConverter jackson2HttpMessageConverter() {
-		MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
-		ObjectMapper objectMapper = new ObjectMapper();
-		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-		objectMapper.registerModule(new Jdk8Module());
-		jsonConverter.setObjectMapper(objectMapper);
-		return jsonConverter;
-	}
+    @Bean
+    public MappingJackson2HttpMessageConverter jackson2HttpMessageConverter() {
+        MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.registerModule(new Jdk8Module());
+        jsonConverter.setObjectMapper(objectMapper);
+        return jsonConverter;
+    }
 }
