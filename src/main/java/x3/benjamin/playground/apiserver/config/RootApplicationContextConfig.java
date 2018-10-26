47113c38bba3e8555c2e6ee2a0ca0cd2a9e14f82
Benjamin <byungboo.kim@nhnent.com>
2018-10-26 23:39:57 +0900

    messageBundle 추가

--- src/main/java/x3/benjamin/playground/apiserver/config/RootApplicationContextConfig.java
+++ src/main/java/x3/benjamin/playground/apiserver/config/RootApplicationContextConfig.java
@@ -4,9 +4,12 @@ import org.springframework.beans.factory.config.PropertiesFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.MessageSourceAccessor;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.core.io.ClassPathResource;
 
 import java.io.IOException;
+import java.util.Locale;
 
 /**
  * Created by benjamin on 2017. 2. 14..
@@ -26,4 +29,17 @@ public class RootApplicationContextConfig {
         return bean;
     }
 
+    // TODO 1.
+    @Bean
+    public MessageSourceAccessor messageSourceAccessor() {
+
+        ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+        source.setDefaultEncoding("utf-8");
+        // TODO 2. check property files.
+        source.setBasename("messages/message_bundle");
+
+        // TODO 3. constructor!
+        MessageSourceAccessor accessor = new MessageSourceAccessor(source, Locale.US);
+        return accessor;
+    }
 }
--- src/main/java/x3/benjamin/playground/apiserver/controller/CommonControllerAdvice.java
+++ src/main/java/x3/benjamin/playground/apiserver/controller/CommonControllerAdvice.java
@@ -1,5 +1,7 @@
 package x3.benjamin.playground.apiserver.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,14 +17,20 @@ import java.util.Random;
 @ControllerAdvice
 public class CommonControllerAdvice {
 
+    // TODO 4
+    @Autowired
+    private MessageSourceAccessor messageSourceAccessor;
+
     @ExceptionHandler(NumberFormatException.class)
     @ResponseBody
     public ResponseEntity<Object> handleApiFailedException(NumberFormatException ex) {
 
-        ErrorMessageDto errorMessageDto = new ErrorMessageDto(ex.getMessage(), ex);
+        // TODO 5
+        String message = messageSourceAccessor.getMessage("error.message");
+
+        ErrorMessageDto errorMessageDto = new ErrorMessageDto(message, ex);
         HttpHeaders headers = new HttpHeaders();
         headers.add("transaction-id", String.valueOf(new Random().nextInt()));
-
 
         return new ResponseEntity(errorMessageDto, headers, HttpStatus.INTERNAL_SERVER_ERROR);
     }
