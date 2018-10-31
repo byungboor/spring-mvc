c7a857562ab93bb561dff1d726d28b4ff3ae1ff5
Benjamin <byungboo.kim@nhnent.com>
2018-10-31 22:44:47 +0900

    remove lombok lib

--- src/main/java/x3/benjamin/playground/apiserver/model/ErrorMessageDto.java
+++ src/main/java/x3/benjamin/playground/apiserver/model/ErrorMessageDto.java
@@ -1,15 +1,28 @@
 package x3.benjamin.playground.apiserver.model;
 
-import lombok.Getter;
-
-@Getter
 public class ErrorMessageDto {
 
     private String message;
     private String clazzName;
 
     public ErrorMessageDto(String message, Exception e) {
-        this.message = message;
-        this.clazzName = e.getClass().getCanonicalName();
+        this.setMessage(message);
+        this.setClazzName(e.getClass().getCanonicalName());
     }
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getClazzName() {
+		return clazzName;
+	}
+
+	public void setClazzName(String clazzName) {
+		this.clazzName = clazzName;
+	}
 }
